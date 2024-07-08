93ebbec22e559c69af85537d0977074d0c91eb48
Seohyun Kim <153896425+1etterh@users.noreply.github.com>
2024-07-08 15:26:34 +0900

    delete: deprecated queries
    - merge queries and delete deprecated ones.
    - new query for setup: create database and add dummy datas

--- seohyun_query/db_setting_ksh.sql
+++ seohyun_query/db_setting_ksh.sql
@@ -1,6 +1,8 @@
-DROP DATABASE if EXISTS sh_testdb;
-CREATE DATABASE sh_testdb;
-USE sh_testdb;
+DROP DATABASE IF EXISTS ksh_test;
+
+CREATE DATABASE ksh_test;
+
+USE ksh_test;
 
 CREATE TABLE tbl_member (
   member_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -118,7 +120,7 @@ CREATE TABLE tbl_skill (
   skill_id INT PRIMARY KEY AUTO_INCREMENT,
   url VARCHAR(255),
   tag_id INT NOT NULL,
-  member_id INT NOT NULL
+  profile_id INT NOT NULL
 );
 
 CREATE TABLE tbl_question (
@@ -186,7 +188,7 @@ ALTER TABLE tbl_contest ADD FOREIGN KEY (contest_post_id) REFERENCES tbl_post (p
 
 ALTER TABLE tbl_skill ADD FOREIGN KEY (tag_id) REFERENCES tbl_tag (tag_id) ON DELETE CASCADE;
 
-ALTER TABLE tbl_skill ADD FOREIGN KEY (member_id) REFERENCES tbl_profile (profile_member_id);
+ALTER TABLE tbl_skill ADD FOREIGN KEY (profile_id) REFERENCES tbl_profile (profile_member_id);
 
 ALTER TABLE tbl_question ADD FOREIGN KEY (recruit_post_id) REFERENCES tbl_recruit (recruit_post_id);
 
@@ -205,8 +207,6 @@ ALTER TABLE tbl_post_report ADD FOREIGN KEY (post_id) REFERENCES tbl_post (post_
 ALTER TABLE tbl_post_tag ADD FOREIGN KEY (post_id) REFERENCES tbl_post (post_id);
 
 ALTER TABLE tbl_post_tag ADD FOREIGN KEY (tag_id) REFERENCES tbl_tag (tag_id) ON DELETE CASCADE;
-
-
 
 -- 회원 삽입
 INSERT INTO tbl_member (member_id, password, email, created_at, updated_at, active_yn) VALUES
@@ -256,7 +256,7 @@ INSERT INTO tbl_tag (tag_id, tag_name, tag_type) VALUES
 (8, 'full-end', 'position');
 
 -- 프로필 별 skill 삽입
-INSERT INTO tbl_skill(skill_id, url, tag_id, member_id) VALUES
+INSERT INTO tbl_skill(skill_id, url, tag_id, profile_id) VALUES
 (1, 'github/spring_boot',1,1),
 (2, 'github/mariaDB',2,1),
 (3, 'github/java',3,1),
@@ -337,12 +337,13 @@ VALUES ('master', NOW(), NOW(), TRUE),
        ('guest', NOW(), NOW(), TRUE);
 
 -- 회원 별 권한
-INSERT INTO tbl_member_auth(member_id, auth_id) VALUES 
-(1,1),(1,2),(1,3),(1,4),
-(2,3),(2,4),
-(3,1),(3,2),(3,3),(3,4),
-(4,2),(4,1),(4,3),(4,4),
-(5,3),(5,4);
+INSERT INTO tbl_member_auth(member_id, auth_id) 
+VALUES 
+(1,1),
+(2,3),
+(3,5),
+(4,2),
+(5,3);
 
 -- 게시글
 -- 멤버가 게시글을 쓰는 INSERT 문
@@ -444,3 +445,129 @@ INSERT INTO tbl_scrap_like (member_id, post_id, flag, created_at) VALUES
 (3, 3, FALSE, NOW()),
 (5, 8, TRUE, NOW()),
 (10, 3, TRUE, NOW());
+
+DELIMITER //
+
+CREATE OR REPLACE TRIGGER after_create_member
+        AFTER INSERT ON tbl_member
+        FOR EACH ROW
+BEGIN
+        INSERT INTO tbl_profile (profile_member_id, introduction, profile_image, nickname, soft_skill, created_at, updated_at, active_yn)
+        VALUES (NEW.member_id, NULL, NULL, NULL, NULL, NOW(), NOW(), FALSE);
+        INSERT INTO tbl_member_auth (member_id, auth_id)
+        VALUES (NEW.member_id, 4);
+END //
+
+DELIMITER ;
+
+--------------------------------------------
+DELIMITER //
+
+CREATE OR REPLACE TRIGGER after_delete_account
+        AFTER UPDATE ON tbl_member
+        FOR EACH ROW
+BEGIN
+        -- 활성화된 유저라면
+        IF (NEW.active_yn <> OLD.active_yn) AND (NEW.active_yn = FALSE) THEN
+                -- 프로필 비활성화
+                UPDATE tbl_profile
+                SET active_yn = FALSE, updated_at = NOW()
+                WHERE profile_member_id = NEW.member_id;
+        END IF;
+END //
+
+DELIMITER ;
+
+---------------------------------------------------------
+DELIMITER //
+
+CREATE OR REPLACE TRIGGER after_enroll_profile
+        AFTER UPDATE ON tbl_profile
+        FOR EACH ROW
+BEGIN
+        IF NEW.active_yn = 1 AND OLD.active_yn = 0 THEN
+                UPDATE tbl_member_auth
+                SET auth_id = 3
+                WHERE member_id = NEW.profile_member_id;
+        END IF;
+END //
+
+DELIMITER ;
+
+--------------------------------------------------------
+DELIMITER //
+
+CREATE OR REPLACE TRIGGER after_post_for_contest
+        AFTER INSERT ON tbl_post
+        FOR EACH ROW
+BEGIN
+        IF NEW.post_type = 'contest' THEN
+                INSERT INTO tbl_contest (contest_post_id, deadline)
+                VALUES(NEW.post_id, NOW());
+        END IF;
+END //
+
+DELIMITER ;
+
+-----------------------------------------------------------
+DELIMITER //
+
+CREATE OR REPLACE TRIGGER after_insert_team_member
+AFTER INSERT ON tbl_team_member
+FOR EACH ROW
+BEGIN
+    -- tbl_request에 NEW.member_id와 NEW.team_id가 있는지 확인
+    IF EXISTS (SELECT 1 FROM tbl_request WHERE member_id = NEW.member_id AND team_id = NEW.team_id) THEN
+        -- 조건이 만족되면 tbl_request에서 해당 레코드 삭제
+        DELETE FROM tbl_request 
+        WHERE member_id = NEW.member_id AND team_id = NEW.team_id;
+    END IF;
+END//
+
+DELIMITER ;
+
+-------------------------------------------------------
+DELIMITER //
+
+CREATE OR REPLACE TRIGGER trg_update_post_status
+AFTER INSERT ON tbl_post_report
+FOR EACH ROW
+BEGIN
+    -- 동일 post_id의 report 횟수 카운트
+    DECLARE report_count INT;
+    
+    SELECT COUNT(*)
+    INTO report_count
+    FROM tbl_post_report
+    WHERE post_id = NEW.post_id;
+    
+    -- report 횟수가 5회 이상이면 post의 active_yn 업데이트
+    IF report_count >= 5 THEN
+        UPDATE tbl_post
+        SET active_yn = 0
+        WHERE post_id = NEW.post_id;
+    END IF;
+END//
+
+DELIMITER ;
+
+-----------------------------------------------------------
+DELIMITER // 
+
+CREATE OR REPLACE TRIGGER after_member_report
+    AFTER INSERT
+    ON tbl_member_report
+    FOR EACH ROW
+BEGIN
+        if (SELECT COUNT(*)
+            FROM tbl_member_report
+            WHERE reported_id = NEW.reported_id)
+            >= 5
+        then 
+                  UPDATE tbl_member
+                  SET active_yn = FALSE
+                  WHERE member_id = NEW.reported_id;
+        END if;                
+END//
+
+DELIMITER ;
